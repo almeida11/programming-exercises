d2519349ff364e534387ba52a4019952813cdf05
Mateus Almeida <contato.mat55@gmail.com>
2022-11-01 23:04:53 -0300

    feat: challenge 01
    init repo with fist challenger

---
+++ challenge_01.py
@@ -0,0 +1,59 @@
+class Produtos():
+    def __init__(self):
+        self.id = id
+        self.lista_de_produtos = {'Café': 100, 'Açúcar': 200, 'MATEUS': 0}
+
+        if(not self.lista_de_produtos): raise Exception('Lista de Produtos Vazia')
+
+    def listaProdutos(self):
+        lista = 'ID - PRODUTO - PREÇO'
+        for n, i in enumerate(self.lista_de_produtos):
+            if((n + 1) <= len(self.lista_de_produtos)): lista += '\n'
+            lista += ('{} - {} | R$ {}'.format((n + 1), i, self.lista_de_produtos[i]))
+        return lista
+
+    def pegaId(self):
+        id = int(input('\nPassa o produto ai carai: '))
+        if((id <= 0) or (id > len(self.lista_de_produtos))):
+            print('Passe um ID correto!')
+            id = Produtos.pegaId(self)
+        return id
+
+    def pegaQuantidade(self):
+        quantidade = int(input('\nPassa a quantidade ai carai: '))
+        if((quantidade < 0)):
+            print('Passe uma quantidade correta!')
+            quantidade = Produtos.pegaQuantidade(self)
+        return quantidade
+
+    def nomeProduto(self, id = 0):
+        for n, i in enumerate(self.lista_de_produtos):
+            if((n + 1) == id):
+                return i
+        return 'Error'
+
+    def valorProduto(self, id = 0):
+        preco = 0
+        for n, i in enumerate(self.lista_de_produtos):
+            if((n + 1) == id):
+                preco = self.lista_de_produtos[i]
+        return preco
+
+    def valorFrete(self, quantidade = 0):
+        if(quantidade >= 1001): return 240
+        elif(quantidade >= 101): return 120
+        elif(quantidade >= 11): return 60
+        elif(quantidade >= 0): return 30
+        return 0
+
+def main():
+    lojaDoMateus = Produtos()
+    print(lojaDoMateus.listaProdutos())
+    id = lojaDoMateus.pegaId()
+    quantidade = lojaDoMateus.pegaQuantidade()
+    nome = (lojaDoMateus.nomeProduto(id))
+    valor = (lojaDoMateus.valorProduto(id))
+    frete = (lojaDoMateus.valorFrete(quantidade))
+    print('Nome do produto: {}, valor do produto: {}, valor do frete: {}.'.format(nome, valor, frete))
+
+main()
