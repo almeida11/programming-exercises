f576e437bd027cd5d363b3affa87e954939a374c
Mateus Almeida <contato.mat55@gmail.com>
2022-11-06 00:00:55 -0300

    feat: rework challenger 2

--- challenge_02.py
+++ challenge_02.py
@@ -1,108 +1,74 @@
-class Loja():
-    def _init_(self, nome):
+#Painel de produtos e preços
+print('''
+Bem-vindo a Sorveteria do Mateus de Almeida
+______________________________________________CARDÁPIO_________________________________________________
+_______________________________________________________________________________________________________
+|  CÓDIGO  |      DESCRIÇÃO       |  Tamanho P (500ml)  |  Tamanho M (1000ml)  |  Tamanho G (1500ml)  |
+|---------------------------------|---------------------|----------------------|----------------------|
+|    TR    | Sabores Tradicionais |       R$ 6,00       |       R$ 10,00       |       R$ 18,00       |
+|    ES    | Sabores Especiais    |       R$ 7,00       |       R$ 12,00       |       R$ 21,00       |
+|    PR    | Sabores Premium      |       R$ 8,00       |       R$ 14,00       |       R$ 24,00       |
+|----------|----------------------|---------------------|----------------------|----------------------|
+''')
+class Sorveteria():
+    def __init__(self):
         self.preco_total = 0
-        self.op = 'S'
-        print('Bem-vindos a Sorveteria do {}'.format(nome))
-        self.lista_de_produtos = {
+        self.pedido2 = ''
+        #dicionário com produtos e preços
+        self.lista_produtos = {
             'TR': {'Descrição': ['Sabores Tradicionais', 'Tradicional'], 'Preços': {'P': 6, 'M': 10, 'G': 18}},
             'ES': {'Descrição': ['Sabores Especiais', 'Especial'], 'Preços': {'P': 7, 'M': 12, 'G': 21}},
             'PR': {'Descrição': ['Sabores Premium', 'Premium'], 'Preços': {'P': 10, 'M': 14, 'G': 24}},
-            #'MS': {'Descrição': ['Sabores Master', 'Master'], 'Preços': {'P': 500, 'M': 750, 'G': 1000}}
         }
 
-        desc_len = 0
-        for i in self.lista_de_produtos:
-            if desc_len < len(self.lista_de_produtos[i]['Descrição'][0]):
-                desc_len = len(self.lista_de_produtos[i]['Descrição'][0])
-
-        cod_text = 'Código'
-        cod_len = len(cod_text)
-
-        desc_text = 'Descrição'
-        desc_text = ' ' * int((desc_len - (len(desc_text))) / 2) + desc_text + ' ' * int((desc_len - (len(desc_text))) / 2)
-        desc_text += ' ' if (len(desc_text) < (desc_len + 2)) else ''
-        
-        tamp_text = 'Tamanho P (500ml)'
-        tamm_text = 'Tamanho M (1000ml)'
-        tamg_text = 'Tamanho G (1500ml)'
-
-        title = '| {} | {} | {} | {} | {} |'.format(cod_text, desc_text, tamp_text, tamm_text, tamg_text)
-        dash = '-'*int((len(title) - 8)/2) + 'Cardápio' + '-'*int((len(title) - 8)/2)
-        if len(dash) < len(title): dash += '-'
-
-        print(dash)
-        print(title)
-
-        for i in self.lista_de_produtos:
-            print('|' + ' ' * int(1 * (cod_len / 2)) + i + ' ' * (1 * int(cod_len / 2)) + '|', end='')
-            desc_temp_len = len(self.lista_de_produtos[i]['Descrição'][0])
-            text = (' ' * (1 + int((desc_len - desc_temp_len) / 2)) + self.lista_de_produtos[i]['Descrição'][0] + ' ' * (1 + int((desc_len - desc_temp_len) / 2)))
-            text += ' |' if (len(text) < (desc_len + 2)) else '|'
-            print(text, end='')
-
-            for j, k in enumerate(self.lista_de_produtos[i]['Preços']):
-                if j == 0: tam_len = len(tamp_text)
-                elif j == 1: tam_len = len(tamm_text)
-                elif j == 2: tam_len = len(tamg_text)
-                preco = 'R$ {},00'.format(self.lista_de_produtos[i]['Preços'][k])
-                tam_temp_len = len(str(preco))
-                text = ' ' * (1 + int((tam_len - tam_temp_len) / 2)) + str(preco) + ' ' * (1 + int((tam_len - tam_temp_len) / 2))
-                text += ' |' if (len(text) < (tam_len + 2)) else '|'
-                if(j != 2): print(text, end='')
-                elif(j == 2): print(text)
-    
-    def getTamanho(self):
-        self.tamanho = input('Entre com o TAMANHO do pote desejado (P/M/G): ')
-        self.tamanho = self.tamanho.upper()
-        if (self.tamanho == 'P' or self.tamanho == 'M' or self.tamanho == 'G'): return
-        print('!!!!!! TAMANHO DE POTE INVÁLIDO !!!!!!')
-        Loja.getTamanho(self)
-    
-    def getCodigo(self):
-        codigos = ''
-        for i, j in enumerate(self.lista_de_produtos):
-            if (i > 0):
-                codigos += '/'
-            codigos += str(j)
-
-        self.codigo = input('Entre com o CÓDIGO do pote desejado ({}): '.format(codigos))
-        self.codigo = self.codigo.upper()
-    
-        for j in self.lista_de_produtos:
-            if(str(self.codigo) == str(j)): return
-
-        print('!!!!!! CÓDIGO DE SORVETE INVÁLIDO !!!!!!')
-        Loja.getCodigo(self)
-
-    def getOp(self):
-        self.op = input('Desja pedir mais alguma coisa? (S/N): ')
-        self.op = self.op.upper()
-        if(self.op == 'S' or self.op == 'N'): return
-        print('!!!!!! OPÇÃO INVÁLIDA !!!!!!')
-        Loja.getOp(self)
-
-    def pedido(self):
+    def pegaPedido(self):
+        codigos = ""
+        #recebe o tamanho do pote de sorvete
+        self.tamanho = input("Entre com o TAMANHO do pote desejado (P/M/G): ").upper().split()[0] 
+        if (self.tamanho == 'P' or self.tamanho == 'M' or self.tamanho == 'G'): 
+            for i, j in enumerate(self.lista_produtos):
+                if (i > 0):
+                    codigos += '/'
+                codigos += str(j)
+            #recebe o codigo do pote de sorvete
+            self.codigo = input("Entre com o CÓDIGO do pote desejado ({}): ".format(codigos)).upper().split()[0]
+            #verifica se o código digitado está presente no dicionário
+            for j in self.lista_produtos: 
+                if(str(self.codigo)== str(j)): return
+            print("!!!!!! TAMANHO OU CÓDIGO DE SORVETE INVÁLIDO !!!!!!")
+            #se o código for inválido solicita novamente as informações do pedido
+            Sorveteria.pegaPedido(self) 
+        else:
+            print("!!!!!! TAMANHO OU CÓDIGO DE SORVETE INVÁLIDO !!!!!!")
+            # se o tamanho for inválido solicita novamente as informações do pedido
+            Sorveteria.pegaPedido(self) 
+            
+    def calculaValor(self):
         while(True):
-            Loja.getTamanho(self)
-            Loja.getCodigo(self)
             sabor = ''
             preco = 0
-            for i in self.lista_de_produtos:
+            
+            for i in self.lista_produtos:
                 if(i == self.codigo):
-                    sabor = self.lista_de_produtos[i]['Descrição'][1]
-                    preco = self.lista_de_produtos[i]['Preços'][self.tamanho]
-                    self.preco_total += int(self.lista_de_produtos[i]['Preços'][self.tamanho])
+                    #pega a descrição do sorvete do dicionario
+                    sabor = self.lista_produtos[i]['Descrição'][1] 
+                    #pega o preço do produto do dicionario
+                    preco = self.lista_produtos[i]['Preços'][self.tamanho] 
+                    #soma os valores dos sorvetes pedidos
+                    self.preco_total += int(self.lista_produtos[i]['Preços'][self.tamanho])
             texto = ('Você pediu um sorvete sabor {} de R$ {},00'.format(sabor, preco))
             print(texto)
             print('-' * len(texto))
-            Loja.getOp(self)
-            if(self.op == 'S'): continue
-            elif(self.op == 'N'): break
-    
-    def main(self):
-        Loja.pedido(self)
-        
-        print('O total a ser pago é: R$ {},00'.format(self.preco_total))
+            #pergunta se o cliente quer mais alguma coisa
+            self.pedido2 = input("Deseja pedir mais alguma coisa? (S/N): ").upper().split()[0]
+            #caso queira, chama novamente a função pegaPedido para selecionar os tamanhos
+            if(self.pedido2 == 'S'): 
+                Sorveteria.pegaPedido(self)
+            #caso nao queira para o laço de repetição e o pedido é finalizado
+            elif(self.pedido2 == 'N'): 
+                print("O preço total a ser pago é de R$ {},00".format(self.preco_total))
+                break
 
-lojaDoMateus = Loja('Mateus de Almeida')
-lojaDoMateus.main()
+MinhaSorveteria = Sorveteria()
+MinhaSorveteria.pegaPedido()
+MinhaSorveteria.calculaValor()
