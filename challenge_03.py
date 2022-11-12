f5cb9e531c53d81edb24dd5f638b6020527bcfac
Mateus Almeida <contato.mat55@gmail.com>
2022-11-12 20:29:46 -0300

    fix: type of dic

--- challenge_03.py
+++ challenge_03.py
@@ -1,15 +1,15 @@
 class ServicoLimpeza():
     def __init__(self):
         self.tipo_limpeza = {
-            "B": {"Básica": 1},
-            "C": {"Completa": 1.3}
+            "B": ["Básica", 1],
+            "C": ["Completa", 1.3]
             }
 
         self.adicionais = {
-            0: {"Não desejo mais nada": 0},
-            1: {"Passar 10 peças de roupas - R$ 10,00": 10},
-            2: {"Limpeza de 1 Forno/Micro-ondas - R$ 12,00": 12},
-            3: {"Limpeza de 1 Geladeira/Freezer - R$ 20,00": 20},
+            0: ["Não desejo mais nada", 0],
+            1: ["Passar 10 peças de roupas - R$ 10,00", 10],
+            2: ["Limpeza de 1 Forno/Micro-ondas - R$ 12,00", 12],
+            3: ["Limpeza de 1 Geladeira/Freezer - R$ 20,00", 20],
         }
 
     def pegaMetragem(self):
@@ -37,10 +37,12 @@ class ServicoLimpeza():
                 for i in self.tipo_limpeza:
                     #se o tipo selecionado estiver na lista e for tipo básico atribui o valor multiplicador
                     if (i == self.tipo_limpeza_selecionada and i == "B"):
-                        self.multiplicador_tipo_limpeza = 1
+                        self.multiplicador_tipo_limpeza = self.tipo_limpeza[i][1]
+                        print("O tipo de limpeza selecionada foi: " + self.tipo_limpeza[i][0])
                     #se o tipo selecionado estiver na lista e for tipo completa atribui o valor multiplicador
                     elif (i == self.tipo_limpeza_selecionada and i == "C"):
-                        self.multiplicador_tipo_limpeza = 1.30
+                        self.multiplicador_tipo_limpeza = self.tipo_limpeza[i][1]
+                        print("O tipo de limpeza selecionada foi: " + self.tipo_limpeza[i][0])
                 #verifica se o valor digitado é válido para seguir o programa
                 if (self.tipo_limpeza_selecionada == "B" or self.tipo_limpeza_selecionada == "C"):
                     return
@@ -51,23 +53,36 @@ class ServicoLimpeza():
 
     def pegaAdicionaisLimpeza(self):
         while True:
-            try:                
+            try:
+                print("-" * 52)
+                print("DESEJA ALGUM ADIDICIONAL:")
+                print("-" * 52)
+                #mostra os adicionais na tela
+                for i in self.adicionais:
+                    ID = str(i)
+                    print(ID + "-" + (self.adicionais[i])[0])
+                #pega o adicional desejado          
                 self.adicional_selecionado = int(input("Selecione um adicional: "))
                 for i in self.adicionais:
+                    #se o adicional for 1, mostra o adicional selecionado e determina o valor a somar
                     if (i == self.adicional_selecionado and i == 1):
-                        print("1")
+                        print((self.adicionais[i])[0] + " selecionada!")
+                        print("-" * 52)
+                    #se o adicional for 1, mostra o adicional selecionado e determina o valor a somar
                     elif (i == self.adicional_selecionado and i == 2):
-                        print("2")
+                        print((self.adicionais[i])[0] + " selecionada!")
+                        print("-" * 52)
+                    #se o adicional for 1, mostra o adicional selecionado e determina o valor a somar
                     elif (i == self.adicional_selecionado and i == 3):
-                        print("3")
+                        print((self.adicionais[i])[0] + " selecionada!")
+                        print("-" * 52)
                 if (self.adicional_selecionado == 0):
                     return
-                print("Opção Inválida!")
             except:
                 print("Adicional não existe!!")
-        print((self.adicionais[1])["Passar 10 peças de roupas - R$ 10,00"])
 
 #total = metragem * tipo + adicionais
 LimpezaAlmeida = ServicoLimpeza()
-
+LimpezaAlmeida.pegaMetragem()
+LimpezaAlmeida.pegaTipoLimpeza()
 LimpezaAlmeida.pegaAdicionaisLimpeza()
