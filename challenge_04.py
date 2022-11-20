053ad5d116a162841e3b75a84140e22d434049e4
almeida11 <mateus.almeidagf@gmail.com>
2022-11-19 23:20:49 -0300

    code error dict

--- challenge_04.py
+++ challenge_04.py
@@ -1,26 +1,86 @@
-class Cadastrador():
-    def __init__(self):
-        dados_funcionario = {
-            "ID": 0,
-            "NOME": "",
-            "SETOR": "",
-            "SALARIO": 0
-        }
-    def cadastra_funcionario(self, ID):
-        self.ID = ID
-        while True:
-            try:
-                nome_funcionario = str(input("Entre com o nome do funcionário: ")).upper()
-                setor_funcionario = str(input("Entre com o setor do funcionário: ")).upper()
-                salario_funcionario = float(input("Entre com o salário do funcionário: "))
-                if (type(nome_funcionario)) == str: continue
-                if (type(setor_funcionario)) == str: continue
-                if (type(salario_funcionario)) == float: continue
-                else:
-                    print("Valores digitados não são válidos!")
-            except ValueError: pass
 
-    def visualiza_funcionario(self):
-        pass
-    def remove_funcionario(self):
-        pass
+lista_funcionarios = []
+#cadastro de funcionario
+def menuPrograma():
+    matricula_funcionario = 0
+    while True:
+        try:
+            print("""
+    1) Cadastrar Funcionário
+    2) Visualizar Funcionário(s)
+    3) Remover Funcionário
+    4) Sair
+            """)
+        
+            selecao = int(input("Entre com a opção: "))
+            if selecao == 1:
+                matricula_funcionario = matricula_funcionario + 1
+                cadastrarFuncionario(matricula_funcionario)
+            elif selecao == 2:
+                visualizarFuncionario()
+            elif selecao == 3:
+                removerFuncionario()
+            elif selecao == 4:
+                break
+            else:
+                print("Opção Inválida!")
+                menuPrograma()
+        except ValueError:
+            print("Digite um valor válido!")
+def cadastrarFuncionario(ID):
+    print("---JANELA DE CADASTRO DE FUNCIONÁRIO---")
+    print(f"A matrícula do funcionário é: {ID}")
+    nome = input("Entre com o NOME do funcionário: ")
+    setor = input("Entre com o SETOR do funcionário: ")
+    salario = input("Entre com o SALÁRIO do funcionário: ")
+
+    dados_funcionario = {
+        "MATRICULA": ID,
+        "NOME": nome,
+        "SETOR": setor,
+        "SALARIO": salario
+    }
+    lista_funcionarios.append(dados_funcionario)
+#visualização de informações do funcionário
+def visualizarFuncionario():
+    while True:
+        try:
+            print("""
+            1) Consultar Todas as Funcionários
+            2) Consultar Funcionário por Id
+            3) Consultar Funcionário(s) por Setor
+            4) Retornar 
+                    """)
+            selecao = int(input("Entre com a opção: "))
+            #mostrar todos os funcionarios
+            if selecao == 1:
+                print(lista_funcionarios)
+                for i in lista_funcionarios:
+                    for keys in i:
+                        print(f"{keys}: ", lista_funcionarios[0])
+                        for value in keys:
+                            print(value)
+                    
+
+            #mostrar funcionario por ID
+            elif selecao == 2:
+                pass
+            #mostrar funcionarios por setor
+            elif selecao == 3:
+                pass
+            #retornar ao menu
+            elif selecao == 4:
+                break
+            else:
+                print("Opção Inválida!")
+                visualizarFuncionario()
+        except ValueError:
+            print("Digite um valor válido!")
+#remover dados de um funcionário
+def removerFuncionario():
+    pass
+
+menuPrograma()
+cadastrarFuncionario()
+visualizarFuncionario()
+removerFuncionario()
